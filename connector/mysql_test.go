741d820fa3f4a0e26600b7cd0c05387d5ac1216d
leiguoguo <leiguoguo@oneniceapp.com>
2017-10-30 13:37:49 +0800

    transfer orm/mysql to connector

--- connector/mysql_test.go
+++ connector/mysql_test.go
@@ -1,4 +1,4 @@
-package orm
+package connector
 
 import (
 	"database/sql"
@@ -32,8 +32,9 @@ func init() {
 			Password: "Cb84eZaa229ddnm",
 			Database: "kkgoo",
 
-			MaxIdle: 4,
-			MaxOpen: 4,
+			MaxIdle:     4,
+			MaxOpen:     4,
+			MaxLifeTime: time.Second,
 		},
 		"cluster2": MysqlConf{
 			Addr:     "bdb.slave.kkgoo.niceprivate.com:3306",
@@ -41,8 +42,9 @@ func init() {
 			Password: "Cb84eZaa229ddnm",
 			Database: "kkgoo",
 
-			MaxIdle: 4,
-			MaxOpen: 4,
+			MaxIdle:     4,
+			MaxOpen:     4,
+			MaxLifeTime: time.Second,
 		},
 	}
 
@@ -60,6 +62,47 @@ func Test_pool(t *testing.T) {
 	)
 	db, _ = GetMysql("cluster1")
 	rows, _ = db.Query("DESC kk_user")
+
+	for rows.Next() {
+		field_desc := new(FieldDesc)
+		rows.Scan(&field_desc.Field, &field_desc.Type, &field_desc.Null, &field_desc.Key, &field_desc.Default, &field_desc.Extra)
+		fields = append(fields, field_desc)
+	}
+
+	t.Logf("+------------------+--------------------------------+------+-----+---------+----------------+")
+	t.Logf("| Field            | Type                           | Null | Key | Default | Extra          |")
+	t.Logf("+------------------+--------------------------------+------+-----+---------+----------------+")
+	for _, field := range fields {
+		t.Logf("| %-16s | %-30s | %-4s | %-3s | %-7s | %-14s |\n", field.Field, field.Type, field.Null, field.Key, field.Default, field.Extra)
+	}
+	t.Logf("+------------------+--------------------------------+------+-----+---------+----------------+")
+
+	//t.Fail()
+}
+
+func Test_Idle(t *testing.T) {
+	if skipMysql {
+		return
+	}
+	var (
+		db     *sql.DB
+		rows   *sql.Rows
+		fields []*FieldDesc
+		err    error
+	)
+	if db, err = GetMysql("cluster1"); err != nil {
+		t.Logf("get mysql failed: %s", err)
+		t.Fail()
+	}
+	if _, err = db.Exec("SET wait_timeout=2"); err != nil {
+		t.Logf("set wait_timeout failed: %s", err)
+		t.Fail()
+	}
+	time.Sleep(2e9 + 1e8)
+	if rows, err = db.Query("DESC kk_user"); err != nil {
+		t.Logf("desc kk_user failed: %s", err)
+		t.Fail()
+	}
 
 	for rows.Next() {
 		field_desc := new(FieldDesc)
