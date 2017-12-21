68ecf18fce772a9ce5b5f5fedf6fc07b3545dec7
leiguoguo <leiguoguo@oneniceapp.com>
2017-12-21 18:42:16 +0800

    add IsEmpty detect method

--- util/type.go
+++ util/type.go
@@ -6,6 +6,28 @@ import (
 	"strconv"
 )
 
+func Is_empty(v interface{}) bool {
+	return VIs_empty(reflect.ValueOf(v))
+}
+
+func VIs_empty(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
+	return false
+}
+
 func PAs_string(v interface{}) string {
 	return VAs_string(reflect.Indirect(reflect.ValueOf(v)))
 }
